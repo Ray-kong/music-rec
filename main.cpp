09143e96bcea9a6e1a2bcd9345759b2561cf839d
RayKong <ray.hyun.kong@gmail.com>
2023-12-04 17:13:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "lib/include/ApplicationController.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    std::string inputDirectoryPath;
+
+    if(argc == 2) {
+        inputDirectoryPath = argv[1];
+    } else {
+        std::cout << "Please enter the path to the directory containing the input files: ";
+        std::cin >> inputDirectoryPath;
+    }
+
+    ApplicationController controller;
+    if(controller.setUp(inputDirectoryPath)) {
+        controller.run();
+    }
+
 }
